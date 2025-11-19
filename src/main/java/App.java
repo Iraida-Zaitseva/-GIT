c464129bb8475731c036c5137cc01cf9ee140056
Iraida Zaitseva <zaytseva.iraida@bk.ru>
2025-11-19 21:29:10 +0300

    Решение для lesson-1

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -5,8 +5,14 @@ public class App {
     public static void main(String[] args) {
         ApplicationContext applicationContext =
                 new AnnotationConfigApplicationContext(AppConfig.class);
-        HelloWorld bean =
-                (HelloWorld) applicationContext.getBean("helloworld");
-        System.out.println(bean.getMessage());
+
+        HelloWorld hello1 = (HelloWorld) applicationContext.getBean("helloworld");
+        HelloWorld hello2 = (HelloWorld) applicationContext.getBean("helloworld");
+
+        Cat cat1 = (Cat) applicationContext.getBean("cat");
+        Cat cat2 = (Cat) applicationContext.getBean("cat");
+
+        System.out.println("HelloWorld singleton check: " + (hello1 == hello2));
+        System.out.println("Cat prototype check: " + (cat1 == cat2));
     }
 }
